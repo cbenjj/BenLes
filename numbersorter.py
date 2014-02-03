38480b6804671a32a1d956489fb9f7e21357b2aa
Ben Strauber <ben.strauber@gmail.com>
2014-02-02 20:04:49 -0800

    fixed the number sorter

--- numbersorter.py
+++ numbersorter.py
@@ -3,11 +3,14 @@ import sys
 def main(argv):
 	if (len(sys.argv) != 2):
 		sys.exit("enter one and only one number, you idiot!")
-	if ((sys.argv[1]).isdigit() == False):
-		sys.exit("learn what an integer is, then come back and enter one.") 
-	if (int(sys.argv[1]) % 2 == 0):
+	try:
+		inputNumber = int(sys.argv[1])
+	except ValueError:
+		sys.exit("learn what an integer is, then come back and enter one.")
+
+	if (inputNumber % 2 == 0):
 		sys.exit("the number you entered is even.")
-	if (int(sys.argv[1]) % 2 == 1):
+	if (inputNumber % 2 == 1):
 		sys.exit("the number you entered is odd.")
 
 if __name__ == "__main__":
